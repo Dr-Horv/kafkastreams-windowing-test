baa793ae88be7f9103790b1415f0fd112d5517fb
Johan Horvius <drhorv@gmail.com>
2019-09-05 23:20:35 +0200

    Initial stuff

---
+++ src/main/java/se/horv/kafkatest/KafkaWindowingTestApplication.java
@@ -0,0 +1,11 @@
+package se.horv.kafkatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWindowingTestApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaWindowingTestApplication.class, args);
+  }
+}
---
+++ src/test/java/se/horv/kafkatest/SimpleKafkaTest.java
@@ -0,0 +1,19 @@
+package se.horv.kafkatest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@EmbeddedKafka
+public class SimpleKafkaTest {
+
+
+    @Test
+    public void help() {
+
+    }
+}
